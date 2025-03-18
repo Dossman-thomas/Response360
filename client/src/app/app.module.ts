baafa1e5cbf1a1fb67eca895852e627a56781004
Dossman-thomas <tdossman.development@gmail.com>
2025-03-18 08:27:26 -0400

    login component created, including html to test auth service functionalities..

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 // Services
 import { CryptoService } from './services/crypto.service';
@@ -12,17 +13,18 @@ import { CryptoApiService } from './services/crypto-api.service';
 // Components
 import { AppComponent } from './app.component';
 import { TestLoginComponent } from './pages/test-login/test-login.component';
-// import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestLoginComponent,
-    // LoginComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     ToastrModule.forRoot()
