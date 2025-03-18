d75507dff2bf0f3c91d6a2de7c0e7540a48d4652
Dossman-thomas <tdossman.development@gmail.com>
2025-03-18 08:45:09 -0400

    installed bootstrap in client directory and added it to angular.json to be used in the app.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestLoginComponent } from './pages/test-login/test-login.component'; 
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '/super-admin-login', component: LoginComponent },
+  { path: 'super-admin-login', component: LoginComponent },
   // You can add more routes here
   { path: '', redirectTo: '/super-admin-login', pathMatch: 'full' },
-  { path: '/dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
