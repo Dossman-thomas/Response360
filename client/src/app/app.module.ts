b9cf9838b59b4039894b6a157fafb631a3deb912
Dossman-thomas <tdossman.development@gmail.com>
2025-03-19 09:29:30 -0400

    configured angular.json to use a different port so this project doesn't conflict with my last project and local storage is unique to port 4201 not the default 4200.  Also, gave index.html a title to fit the project.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // Services
 import { CryptoService } from './services/crypto.service';
@@ -15,21 +17,24 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    DashboardComponent
-  ],
+  declarations: [AppComponent, LoginComponent, DashboardComponent],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot({
+      timeOut: 1500, // 1.5 seconds
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
   ],
-  providers: [CryptoService, CryptoApiService],
-  bootstrap: [AppComponent]
+  providers: [
+    CryptoService,
+    CryptoApiService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
