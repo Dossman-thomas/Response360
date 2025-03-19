de3c77d07d83e62ba8fad4e0a62a72c6f07f35d5
Dossman-thomas <tdossman.development@gmail.com>
2025-03-18 20:06:11 -0400

    fixed rememberMe bug.  Email and password fields prefill with values set in cookies.

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,19 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   email: string = '';
   password: string = '';
   rememberMe: boolean = false;
   showPassword: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cookieService: CookieService
+  ) {}
+
+  ngOnInit() {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/dashboard']);
+      return;
+    }
+
+    // check if email and password cookies are set
+    // Check for stored credentials in cookies and populate the fields
+    const storedEmail = this.cookieService.get('email');
+    const storedPassword = this.cookieService.get('password');
+    const storedRememberMe = this.cookieService.get('rememberMe');
+
+    console.log('Stored Email:', storedEmail);
+    console.log('Stored Password:', storedPassword);
+    console.log('Remember Me:', this.rememberMe);
+
+    if (storedEmail) {
+      this.email = storedEmail;
+    }
+
+    if (storedPassword) {
+      this.password = storedPassword;
+    }
+
+    if (storedRememberMe) {
+      this.rememberMe = true;
+    }
+  }
 
   onSubmit() {
     // Call login from the AuthService
