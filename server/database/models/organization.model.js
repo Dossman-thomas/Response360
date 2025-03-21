6aea950d9119ec0a9ca447fd455435004411fc4e
Dossman-thomas <tdossman.development@gmail.com>
2025-03-21 06:48:48 -0400

    created organization model and added export to models index to be used by the app

---
+++ server/database/models/organization.model.js
@@ -0,0 +1,63 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.config";
+
+export const OrganizationModel = sequelize.define(
+  "organizations",
+  {
+    org_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    org_name: {
+      type: DataTypes.TEXT, // Encrypted
+      allowNull: false,
+    },
+    org_email: {
+      type: DataTypes.TEXT, // Encrypted
+      allowNull: false,
+    },
+    org_phone_number: {
+      type: DataTypes.TEXT, // Encrypted
+    },
+    org_status: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    org_created_by: {
+      type: DataTypes.UUID,
+    },
+    org_updated_by: {
+      type: DataTypes.UUID,
+    },
+    org_deleted_by: {
+      type: DataTypes.UUID,
+    },
+    org_type: {
+      type: DataTypes.STRING(50),
+    },
+    jurisdiction: {
+      type: DataTypes.STRING(20),
+      validate: {
+        isIn: [['Global', 'National', 'State', 'District']], // ENUM constraint
+      },
+    },
+    org_address: {
+      type: DataTypes.TEXT, // Encrypted
+    },
+    website: {
+      type: DataTypes.TEXT, 
+    },
+    logo: {
+      type: DataTypes.STRING(5000), // URL stored as string
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: "org_created_at",
+    updatedAt: "org_updated_at",
+    deletedAt: "org_deleted_at",
+    paranoid: true, // Soft deletes
+  }
+);
