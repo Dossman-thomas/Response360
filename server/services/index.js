b760f4c183ce7521bb1509b76114f23690a370ce
Dossman-thomas <tdossman.development@gmail.com>
2025-03-24 13:57:45 -0400

    configured organization.controller file to manage the createOrganizationService request and response properly.

--- server/services/index.js
+++ server/services/index.js
@@ -3,3 +3,5 @@ export { encryptService, decryptService } from './common.service.js';
 export { createSuperAdminService } from './super-admin.service.js';
 
 export { loginSuperAdminService } from './auth.service.js';
+
+export { createOrganizationService } from './organization.service.js';
