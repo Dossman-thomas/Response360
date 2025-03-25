94cf401053ae0fc393ed7b60539eb310601a0fef
Dossman-thomas <tdossman.development@gmail.com>
2025-03-25 13:53:12 -0400

    reconfigured app-routing so all routes are children of the layout component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,24 +2,42 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
-
+import { LayoutComponent } from './pages/layout/layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ManageOrganizationsComponent } from './pages/manage-organizations/manage-organizations.component';
 import { OrganizationDetailsComponent } from './pages/organization-details/organization-details.component';
 
-
 const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', redirectTo: '/super-admin-login', pathMatch: 'full' },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'manage-organizations',
+        component: ManageOrganizationsComponent,
+        canActivate: [AuthGuard],
+      },
+      {
+        path: 'organization-details',
+        component: OrganizationDetailsComponent,
+        canActivate: [AuthGuard],
+      },
+    ],
+  },
   { path: 'super-admin-login', component: LoginComponent },
-  // You can add more routes here
-  { path: '', redirectTo: '/super-admin-login', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'manage-organizations', component: ManageOrganizationsComponent, canActivate: [AuthGuard] },
-  { path: 'organization-details', component: OrganizationDetailsComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/super-admin-login' }, // Redirect to the login page if the URL is invalid
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
