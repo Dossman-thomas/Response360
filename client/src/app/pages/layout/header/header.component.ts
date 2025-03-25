321b9b07b4dd6908a6908baa77a12da2b406a665
Dossman-thomas <tdossman.development@gmail.com>
2025-03-25 15:42:02 -0400

    configured header, and added a centered search bar and a caret down that toggles a dropdown with the logout button

--- client/src/app/pages/layout/header/header.component.ts
+++ client/src/app/pages/layout/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  isDropdownOpen: boolean = false;
 
+  constructor(private authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
