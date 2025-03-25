e7ab5e64efcc5e52b58f7183adb3d9af7a6c783d
Dossman-thomas <tdossman.development@gmail.com>
2025-03-25 09:00:02 -0400

    fixed slight login bug where token wasn't being passed to the front end

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,12 +5,17 @@ import { AuthGuard } from './auth/auth.guard';
 
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ManageOrganizationsComponent } from './pages/manage-organizations/manage-organizations.component';
+import { OrganizationDetailsComponent } from './pages/organization-details/organization-details.component';
+
 
 const routes: Routes = [
   { path: 'super-admin-login', component: LoginComponent },
   // You can add more routes here
   { path: '', redirectTo: '/super-admin-login', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'manage-organizations', component: ManageOrganizationsComponent, canActivate: [AuthGuard] },
+  { path: 'organization-details', component: OrganizationDetailsComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
