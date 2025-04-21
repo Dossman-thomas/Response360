57b5c768948be1e0ee35d4a992a067c61460862d
Dossman-thomas <tdossman.development@gmail.com>
2025-04-21 11:45:20 -0400

    logged in user email now displayed in header next to user icon

--- client/src/app/pages/layout/header/header.component.ts
+++ client/src/app/pages/layout/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { CryptoService } from '../../../services/crypto.service';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +10,26 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HeaderComponent {
   isDropdownOpen: boolean = false;
+  userEmail: string = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private cookieService: CookieService,
+    private cryptoService: CryptoService
+  ) {}
+
+  ngOnInit(): void {
+    const storedEmail = this.cookieService.get('email');
+
+    // Decrypt userEmail from cookies
+    if (storedEmail) {
+      try {
+        this.userEmail = this.cryptoService.Decrypt(storedEmail);
+      } catch (err) {
+        console.error('Failed to decrypt user email from cookie:', err);
+      }
+    }
+  }
 
   logout(): void {
     this.authService.logout();
