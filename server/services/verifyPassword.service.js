d214ee4e79e5b422029ac58a08862ed0f4aa79b5
Dossman-thomas <tdossman.development@gmail.com>
2025-04-28 15:07:04 -0400

    updated verifyPassword service to adhere to standards set by backend code review.

--- server/services/verifyPassword.service.js
+++ server/services/verifyPassword.service.js
@@ -1,18 +1,39 @@
 import { UserModel } from '../database/models/index.js';
 import { env } from '../config/index.js';
-import { decryptSensitiveData } from '../utils/index.js';
+import { decryptSensitiveData, createError } from '../utils/index.js';
 import { decryptService } from './index.js';
 
 const pubkey = env.encryption.pubkey;
 
+if(!pubkey){
+  throw createError('Public key is not defined in the environment variables', 500, {
+    code: 'PUBLIC_KEY_NOT_DEFINED',
+  });
+}
+
 export const verifyPasswordService = async (payload) => {
   try {
+    // validate the payload
+    if (!payload) {
+      throw createError('Encrypted payload string is required', 400, {
+        code: 'ENCRYPTED_PAYLOAD_MISSING',
+      });
+    }
+
+    // Check if the payload is a string
+    if (typeof payload !== 'string') {
+      throw createError('Encrypted payload must be a string', 400, {
+        code: 'ENCRYPTED_PAYLOAD_INVALID',
+      });
+    }
+
     // Decrypt the payload
     const decryptedPayload = await decryptService(payload);
+
     if (!decryptedPayload) {
-      const error = new Error('Invalid payload');
-      error.status = 400;
-      throw error;
+      throw createError('Decryption failed', 400, {
+        code: 'DECRYPTION_PAYLOAD_FAILED',
+      });
     }
 
     const { user_email, currentPassword } = decryptedPayload;
@@ -27,9 +48,9 @@ export const verifyPasswordService = async (payload) => {
     });
 
     if (!foundUser) {
-      const error = new Error('User not found.');
-      error.status = 404;
-      throw error;
+      throw createError('User not found', 404, {
+        code: 'USER_NOT_FOUND',
+      });
     }
 
     // Verify the password
@@ -37,9 +58,9 @@ export const verifyPasswordService = async (payload) => {
 
     // If the password does not match, throw an error
     if (!isMatch) {
-      const error = new Error('Invalid password');
-      error.status = 401;
-      throw error;
+      throw createError('Password not match', 401, {
+        code: 'PASSWORD_NOT_MATCH',
+      });
     }
 
     return {
@@ -47,10 +68,9 @@ export const verifyPasswordService = async (payload) => {
       message: 'Password verified successfully.',
     };
   } catch (error) {
-    console.error('Error in verifyPasswordService:', error);
-    return {
-      success: false,
-      message: error.message || 'Failed to verify password',
-    };
+    console.error('Error in verifyPasswordService:', error.message);
+    throw createError('Failed to verify password', 500, {
+      code: 'PASSWORD_VERIFICATION_FAILED'
+    });
   }
 };
